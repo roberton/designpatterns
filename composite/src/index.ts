64ef8569b7863519d314dc61d647d79e39474e63
roberton <rnerici@gmail.com>
2024-12-16 08:39:10 +0000

    implements a container

--- composite/src/index.ts
+++ composite/src/index.ts
@@ -37,14 +37,59 @@ class Item implements InventoryComponent {
   }
 }
 
+class Container implements InventoryComponent {
+  private name: string;
+  private weight: number;
+  private contents: InventoryComponent[];
+
+  constructor(name: string, weight: number) {
+    this.name = name;
+    this.weight = weight;
+    this.contents = [];
+  }
+
+  getName() {
+    const contentsNames = this.contents.map(item => item.getName()).join(',\n\t');
+    return `${this.name} which contains:\n\t${contentsNames}`;
+  }
+
+  getWeight() {
+    const weightOfContents = this.contents.reduce(
+      (total, child) => total + child.getWeight(),
+      0);
+    return this.weight + weightOfContents;
+  }
+
+  add(child: InventoryComponent) {
+    this.contents.push(child);
+  }
+
+  remove(child: InventoryComponent): void {
+    const indexOfChild = this.contents.indexOf(child);
+    this.contents.splice(indexOfChild, 1);
+  }
+
+  getChild(index: number): InventoryComponent {
+    return this.contents[index];
+  }
+}
+
 
 // Create some people with different inventories
 function createCommuter() {
-  const wallet = new Item('wallet', 0.1);
+  const inventory = new Container('Inventory', 0);
+  const wallet = new Item('Wallet', 0.1);
+  const phone = new Item('Phone', 0.3);
 
-  console.log(`Commuter's inventory is: ${wallet.getName()}`);
-  console.log(`The total weight is: ${wallet.getWeight()}`);
+  inventory.add(wallet);
+  inventory.add(phone);
+
+  console.log(`The commuter has:`);
+  console.log(`${inventory.getName()}`);
+  console.log(`In total, the commuter is carrying ${inventory.getWeight()}kg of stuff.`);
 }
+
 
 // Test them all out
 createCommuter();
+
